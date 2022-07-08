26052985e76fe292cee375fd00426c5e3e52d568
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-07-08 15:03:47 +0530

    changes in sidebar

--- acm-member-dashboard/src/App.js
+++ acm-member-dashboard/src/App.js
@@ -1,11 +1,21 @@
-
-
-function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
-}
-
-export default App;
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+ 
+function App(){
+ 
+        return (
+            <div>
+                <Navbar/>
+                <div class="container-fluid" id="main">
+                 <div class="row row-offcanvas row-offcanvas-left">
+                   <Sidebar/>
+                  
+                
+             </div>
+            </div>  
+        </div>  
+        );
+    }
+  
+export default App
